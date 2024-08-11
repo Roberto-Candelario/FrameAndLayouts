b6095bb8d2e4c5f82faef337507da2632e0b4eed
Roberto Candelario <r-jc@live.com>
2024-08-10 23:27:42 -0400

    Initial Commit

---
+++ FrameAndLayouts/FrameAndLayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameAndLayoutsApp.swift
+//  FrameAndLayouts
+//
+//  Created by Roberto Candelario on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FrameAndLayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrameAndLayoutsTests/FrameAndLayoutsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrameAndLayoutsTests.swift
+//  FrameAndLayoutsTests
+//
+//  Created by Roberto Candelario on 8/10/24.
+//
+
+import Testing
+@testable import FrameAndLayouts
+
+struct FrameAndLayoutsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
