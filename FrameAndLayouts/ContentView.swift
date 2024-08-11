967ec2666312ca758446245242e6bd9e8fc56fcf
Roberto Candelario <r-jc@live.com>
2024-08-10 23:49:33 -0400

    Completed the Frame and Layout Example reading

--- FrameAndLayouts/ContentView.swift
+++ FrameAndLayouts/ContentView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                //.background(Color.red) // Applies to what comes before, just the hello world
+               //.frame(width: 200, height: 50)
+               // .background(Color.blue) // Applies to the frame, what came before this code
+           // Spacer()// Spacers add too much space and moves the stuff before this line to the top of the screen and the code below to the bottom of the screen
+                .padding() // By adding padding, it adds default values of space
+            HStack {
+                Button("One") {}
+                Button("Two") {}
+                Button("Three") {}
+                
+            }
         }
+        .frame(width: 300, alignment: .leading)
         .padding()
     }
 }
